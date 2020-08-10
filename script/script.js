1baf1b517741982270d14e2cd7fdd0e56f759594
Katya <13gordienkok@gmail.com>
2020-08-10 15:57:36 +0300

    git push -u origin master

---
+++ script/script.js
@@ -0,0 +1,9 @@
+alert('Hello frends');
+let money = 5;
+let income = 'string';
+let addExpenses = 'true';
+let deposit = 156;
+let mission = 'Hemo';
+let period;
+console.log(money);
+alert(deposit);
